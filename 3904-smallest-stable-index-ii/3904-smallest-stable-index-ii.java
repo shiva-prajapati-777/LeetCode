2d7dc760a29ade7db786ec1ffe7962357d8c73ad
Shiva Prajapati <sp412457@gmail.com>
2026-09-05 12:36:24 +0530

    Time: 4 ms (88.55%), Space: 133.1 MB (32.44%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
        int n = nums.length;
-       int count =0;
        int [] right = new int[n];
        right[n-1]= nums[n-1];
        for (int i = n-2;i>=0;i--){
